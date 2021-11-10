f48c65666dd3509638ecfe39dbf4adf5c3edef1c
VVP <vvp98@yandex.ru>
2021-11-11 00:28:43 +0300

    add object

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const a = +prompt('Введите первое число');
 const b = +prompt('Введите второе число');
 const myAct = prompt('Введите действие');
 
+const operations = {
+    "+": "sum",
+    "-": "sub",
+    "*": "mult",
+    "/": "div",
+    "%": "rem",
+    "**": "pow",
+};
+
+
 function Calc(myAct, a, b) {
     if (isNaN(a) === true || isNaN(b) === true || a === "" || b === "" || myAct === "" || a === null || b === null || myAct === null) {
         return console.log("Error");
-    } else if (myAct === "+") {
+    } else if (myAct === operations["+"]) {
         return console.log(a + b);
-    } else if (myAct === "-") {
+    } else if (myAct === operations["-"]) {
         return console.log(a - b);
-    } else if (myAct === "*") {
+    } else if (myAct === operations["*"]) {
         return console.log(a * b);
-    } else if (myAct === "/") {
+    } else if (myAct === operations["/"]) {
         return console.log(a / b);
-    } else if (myAct === "%") {
+    } else if (myAct === operations["%"]) {
         return console.log(a % b);
-    } else if (myAct === "**") {
+    } else if (myAct === operations["**"]) {
         return console.log(a ** b);
     } else {
         return console.log("unknown operation");
